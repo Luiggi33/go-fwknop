b51aec7ad82154a684bbc76d5f629e6cf35129c7
Luiggi33 <github@luiggi33.de>
2026-02-21 10:55:12 +0100

    refactor: use bpf filter to reduce noise

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net"
 
 	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
 )
 
 const (
-	DEVICE = "lo"
+	DEVICE         = "lo"
+	KNOCKING_PORT  = 8000
+	KNOCKING_PROTO = "udp"
 )
 
 func main() {
@@ -18,8 +23,32 @@ func main() {
 	}
 	defer handle.Close()
 
+	handle.SetBPFFilter(fmt.Sprintf("%s dst port %d", KNOCKING_PROTO, KNOCKING_PORT))
+
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 	for packet := range packetSource.Packets() {
+		var srcIP net.IP
 
+		if nl := packet.NetworkLayer(); nl != nil {
+			switch v := nl.(type) {
+			case *layers.IPv4:
+				srcIP = v.SrcIP
+			case *layers.IPv6:
+				srcIP = v.SrcIP
+			}
+		}
+
+		var destPort uint16
+
+		if tl := packet.TransportLayer(); tl != nil {
+			switch v := tl.(type) {
+			case *layers.UDP:
+				destPort = uint16(v.DstPort)
+			case *layers.TCP:
+				destPort = uint16(v.DstPort)
+			}
+		}
+
+		fmt.Println(srcIP, " ", destPort)
 	}
 }
