b359d30f88e49c10256eb4a9095c6003f2c70b30
Luiggi33 <github@luiggi33.de>
2026-02-25 20:32:47 +0100

    feat: add config file

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.25.7
 
 require (
 	github.com/google/gopacket v1.1.19 // indirect
+	go.yaml.in/yaml/v3 v3.0.4 // indirect
 	golang.org/x/sys v0.0.0-20190412213103-97732733099d // indirect
 )
--- main.go
+++ main.go
@@ -1,29 +1,73 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net"
+	"os"
+	"strings"
 
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
+	"go.yaml.in/yaml/v3"
 )
 
-const (
-	DEVICE         = "lo"
-	KNOCKING_PORT  = 8000
-	KNOCKING_PROTO = "udp"
-)
+type AccessRule struct {
+	KnockProto string `yaml:"knock_proto"`
+	KnockPort  uint16 `yaml:"knock_port"`
+	OpenProto  string `yaml:"open_proto"`
+	OpenPort   uint16 `yaml:"open_port"`
+}
+
+type Config struct {
+	Device      string       `yaml:"listen_on_interface"`
+	AccessRules []AccessRule `yaml:"access_rules"`
+}
+
+func bpfFilterFromString(config *Config) string {
+	returnString := ""
+	for _, accessRule := range config.AccessRules {
+		returnString += fmt.Sprintf("(%s dst port %d) or ", accessRule.KnockProto, accessRule.KnockPort)
+	}
+	return strings.TrimSuffix(returnString, " or ")
+}
+
+func findMatchingRule(rules []AccessRule, proto string, port uint16) *AccessRule {
+	for _, rule := range rules {
+		if strings.EqualFold(rule.KnockProto, proto) && rule.KnockPort == port {
+			return &rule
+		}
+	}
+	return nil
+}
 
 func main() {
-	handle, err := pcap.OpenLive(DEVICE, 1600, true, pcap.BlockForever)
+	configFile := flag.String("config-file", "config.yaml", "config file that should be used")
+
+	var config Config
+	yamlFile, err := os.ReadFile(*configFile)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Error reading config file: %v ", err)
+	}
+	err = yaml.Unmarshal(yamlFile, &config)
+	if err != nil {
+		log.Fatalf("Error reading in config: %v", err)
+	}
+
+	handle, err := pcap.OpenLive(config.Device, 1600, true, pcap.BlockForever)
+	if err != nil {
+		log.Fatalf("Error starting listener: %v", err)
 	}
 	defer handle.Close()
 
-	handle.SetBPFFilter(fmt.Sprintf("%s dst port %d", KNOCKING_PROTO, KNOCKING_PORT))
+	err = handle.SetBPFFilter(bpfFilterFromString(&config))
+	if err != nil {
+		log.Fatalf("Error setting BPF filter: %v", err)
+	}
+
+	log.Printf("Successfully started knock listener on device \"%s\"\n", config.Device)
 
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 	for packet := range packetSource.Packets() {
@@ -38,17 +82,25 @@ func main() {
 			}
 		}
 
+		var destProto string
 		var destPort uint16
 
 		if tl := packet.TransportLayer(); tl != nil {
 			switch v := tl.(type) {
 			case *layers.UDP:
 				destPort = uint16(v.DstPort)
+				destProto = "udp"
 			case *layers.TCP:
 				destPort = uint16(v.DstPort)
+				destProto = "tcp"
 			}
 		}
 
-		fmt.Println(srcIP, " ", destPort)
+		rule := findMatchingRule(config.AccessRules, destProto, destPort)
+		if rule == nil {
+			continue
+		}
+
+		log.Printf("Valid knock from %s\n", srcIP)
 	}
 }
