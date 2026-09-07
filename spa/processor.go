5f328b9b2d1614da808667101401cebb41df7e25
Luiggi33 <github@luiggi33.de>
2026-09-07 20:11:40 +0200

    fix: ip equals check for ipv6 shortened form

--- spa/processor.go
+++ spa/processor.go
@@ -105,7 +105,7 @@ func (p *Processor) Process(rawPayload []byte, knockPort uint16, srcIP net.IP) (
 		return nil, nil, ErrSPARejected
 	}
 	srcIPStr := string(ciphertextParts[4])
-	if srcIPStr != srcIP.String() {
+	if net.ParseIP(srcIPStr).Equal(srcIP) {
 		log.Printf("processor: source IP in decrypted payload doesn't match actual source IP, somebody tampered!")
 		return nil, nil, ErrSPARejected
 	}
